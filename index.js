f3118fced34ceb00356cd87f99889d5d0c4cb1cb
Penn Curtis <penn.s.m.c@gmail.com>
2023-04-13 09:51:29 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const finishedCards = []
+    for(let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        finishedCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    console.log(finishedCards)
+    return finishedCards
+}
+
+writeCards('Guadalupe', 'Ollie', 'Aki')
+
+
+function countDown(){
+    let count = 10
+    while(count >= 0){
+        console.log(count--)
+    }
+}
